51da4b7337297cf083ce51694990d9c2c49a2b55
focusotter <mtliendo@focusotter.com>
2023-03-15 00:25:36 -0500

    remove log

--- pages/index.tsx
+++ pages/index.tsx
@@ -27,7 +27,6 @@ const fetchTravelList = async (isAuth: boolean) => {
 }
 
 export default function Home() {
-	console.log('the env vars', process.env)
 	const [travelPosts, setTravelPosts] = useState<TravelPost[] | []>([])
 	const [currUser, setCurrUser] = useState<{} | null>(null)
 	const theme = useTheme()
